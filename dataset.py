c1404439b1d1c7d5b7a8951933337117a3f63e5e
Kommareddy Diana Mary <113375291+DianaKommareddy@users.noreply.github.com>
2025-09-04 15:02:36 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -11,10 +11,11 @@ class CustomDataset(Dataset):
         self.G_dir = os.path.join(root_dir, "G")
         self.Y_dir = os.path.join(root_dir, "Y")
         self.image_files = sorted(os.listdir(self.A_dir))
+
         if transform is None:
-            # For grayscale images
+            # Resize input images smaller to 64x64 to reduce memory
             self.transform = transforms.Compose([
-                transforms.Resize((128, 128)),
+                transforms.Resize((64, 64)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.5], std=[0.5])
             ])
@@ -25,18 +26,20 @@ class CustomDataset(Dataset):
         return len(self.image_files)
 
     def __getitem__(self, idx):
-        # Always open as grayscale for model channel agreement
         A_path = os.path.join(self.A_dir, self.image_files[idx])
         E_path = os.path.join(self.E_dir, self.image_files[idx])
         G_path = os.path.join(self.G_dir, self.image_files[idx])
         Y_path = os.path.join(self.Y_dir, self.image_files[idx])
-        # Open as L (grayscale, 1 channel)
+
+        # Load grayscale images (single channel)
         A = Image.open(A_path).convert("L")
         E = Image.open(E_path).convert("L")
         G = Image.open(G_path).convert("L")
         Y = Image.open(Y_path).convert("L")
+
         A = self.transform(A)
         E = self.transform(E)
         G = self.transform(G)
         Y = self.transform(Y)
+
         return A, E, G, Y
