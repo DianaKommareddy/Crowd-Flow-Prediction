16eb58626592185182c41d6d954d3cbdd06007a1
Kommareddy Diana Mary <113375291+DianaKommareddy@users.noreply.github.com>
2025-09-17 15:21:10 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from .BioInspiredModel import BioCrowdFlowModel
-from .restormer_crowd_flow import SharpRestormer  
+from .restormer_crowd_flow import HINT  
 from .Bio_Transformer import BioCrowdFlowModel
